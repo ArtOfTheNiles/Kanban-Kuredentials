aba664be05616c8a9eca2b2c91bb29ea5e09e1d6
Niles Bontrager <artoftheniles@yahoo.com>
2025-01-28 18:37:33 -0500

    Server Functional without Auth

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -1,9 +1,9 @@
 import { Router, Request, Response } from 'express';
-import { User } from '../models/user.js';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+// import { User } from '../models/user.js';
+// import jwt from 'jsonwebtoken';
+// import bcrypt from 'bcrypt';
 
-export const login = async (req: Request, res: Response) => {
+export const login = async (_req: Request, _res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
 };
 
